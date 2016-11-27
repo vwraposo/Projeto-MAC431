b53dfea8feba7ba06d41da4af044b842b290c64e
Victor Wichmann Raposo <vwraposo@gmail.com>
2016-11-27 16:27:30 -0200

    Verificando Overflow

--- ep.cpp
+++ ep.cpp
@@ -14,13 +14,77 @@ int main (int argc, char** argv) {
     // OpenMP initialization
     omp_set_num_threads (num_threads);
 
+    std::atomic<int> over = 0;
     for (int it = 0; it < iter; it++) {
         // Mecher no jeito de iterar
         #pragma omp parallel for schedule (guided, 4)
         for (int i = 0; i < n; i++) 
             for (int j = 0; j < m; j++) 
-                operacao (i, j);
-        
+                over.fetch_add (operacao (i, j));
+
+        if (over > 0) { 
+            #pragma omp parallel for schedule (guided, 4)
+            for (int i = 0; i < n; i++) 
+                for (int j = 0; j < m; j++) {
+                    if (mat2[i][j][R] > 255 * MU) {
+                        ull delta = mat2[i][j][R] - 255*MU / 4;
+                        mat2[i][j][R].store (255*MU);
+                        #pragma omp critcal 
+                        {
+                            if (j+1 < m && mat2[i][j+1][R] + delta > 255*MU)
+                                mat2[i][j+1][R].fetch_add (delta);
+
+                            if (i+1 < n && mat2[i+1][j][R] + delta > 255*MU)
+                                mat2[i+1][j][R].fetch_add (delta);
+
+                            if (j >= 1 && mat2[i][j-1][R] + delta > 255*MU)
+                                mat2[i][j-1][R].fetch_add (delta);
+
+                            if (i >= 1 && mat2[i-1][j][R] + delta > 255*MU) 
+                                mat2[i-1][j][R].fetch_add (delta);
+                        }
+                    }
+                    if (mat2[i][j][G] > 255 * MU) {
+                        ull delta = mat2[i][j][G] - 255*MU / 4;
+                        mat2[i][j][G].store (255*MU);
+                        #pragma omp critcal 
+                        {
+                            if (j+1 < m && mat2[i][j+1][G] + delta > 255*MU)
+                                mat2[i][j+1][G].fetch_add (delta);
+
+                            if (i+1 < n && mat2[i+1][j][G] + delta > 255*MU)
+                                mat2[i+1][j][G].fetch_add (delta);
+
+                            if (j >= 1 && mat2[i][j-1][G] + delta > 255*MU)
+                                mat2[i][j-1][G].fetch_add (delta);
+
+                            if (i >= 1 && mat2[i-1][j][G] + delta > 255*MU) 
+                                mat2[i-1][j][G].fetch_add (delta);
+                        }
+                    }
+                    if (mat2[i][j][B] > 255 * MU) {
+                        ull delta = mat2[i][j][B] - 255*MU / 4;
+                        mat2[i][j][B].store (255*MU);
+                        #pragma omp critcal 
+                        {
+                            if (j+1 < m && mat2[i][j+1][B] + delta > 255*MU)
+                                mat2[i][j+1][B].fetch_add (delta);
+
+                            if (i+1 < n && mat2[i+1][j][B] + delta > 255*MU)
+                                mat2[i+1][j][B].fetch_add (delta);
+
+                            if (j >= 1 && mat2[i][j-1][B] + delta > 255*MU)
+                                mat2[i][j-1][B].fetch_add (delta);
+
+                            if (i >= 1 && mat2[i-1][j][B] + delta > 255*MU) 
+                                mat2[i-1][j][B].fetch_add (delta);
+                        }
+                    }
+                }
+        }
+
+
+
         #pragma omp parallel for schedule (guided, 4)
         for (int i = 0; i < n; i++) 
             for (int j = 0; j < m; j++) {
