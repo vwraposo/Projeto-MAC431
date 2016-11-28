c8ad334d63f625fbeeaa7e7ce8e6327a6c64cec2
Leonardo Padilha <lcfpadilha@gmail.com>
2016-11-28 02:39:39 -0200

    inserindo comentarios e ajustando leitura de imagem

--- ep.cpp
+++ ep.cpp
@@ -1,35 +1,63 @@
+/*******************************************************************/
+/*                    Projeto de MAC0431                           */
+/* Nomes:                                                          */
+/* Leonardo Padilha                             Nº USP: 9298295    */
+/* Lucas Santos                                 Nº USP:            */
+/* Victor da Matta                              Nº USP:            */
+/* Victor Raposo                                Nº USP:            */
+/*******************************************************************/
 #include "ep.hpp"
 
 int main (int argc, char** argv) {
     std::ios::sync_with_stdio (false);
+
+    // Verificamos se os argumentos estão corretos 
     if (argc != 5) {
         fprintf (stderr, "Uso: ./ep  <entrada> <saida> <iter> <num CPU>\n");
         return EXIT_FAILURE;
     }
 
+    // Leitura da imagem cujo nome é dado pelo primeiro argumento 
     read_im (argv[1]);
+
+    // Quantidade de iterações 
     int iter = atoi (argv[3]);
+
+    // Número de threads utilizadas
     int num_threads = atoi (argv[4]);
 
-    // OpenMP initialization
+    // Inicialização do OpenMP 
     omp_set_num_threads (num_threads);
 
+    // Declaração de um inteiro para registrar overflow
     std::atomic<int> over;
     over.store (0);
+
     for (int it = 0; it < iter; it++) {
-        // Mecher no jeito de iterar
+
+        // Executamos, paralelamente e com balanceamento de
+        //carga guided (tamanho mínimo de cada chunk de 4), a 
+        //operação em cada um dos pixels da matriz mat e joga-
+        //mos os resultados em mat2
         #pragma omp parallel for schedule (guided, 4)
         for (int i = 0; i < n; i++) 
             for (int j = 0; j < m; j++) 
                 over.fetch_add (operacao (i, j));
 
+        // Se houver overflow em alguns dos pixels
         if (over > 0) { 
+            // Verificamos em cada pixel qual houve overflow
+            //e corrigimos o valor dele
             #pragma omp parallel for schedule (guided, 4)
             for (int i = 0; i < n; i++) 
                 for (int j = 0; j < m; j++) {
+                    // Se o valor do vermelho deu overflow
                     if (mat2[i][j][R] > 255 * MU) {
                         ull delta = mat2[i][j][R] - 255*MU / 4;
                         mat2[i][j][R].store (255*MU);
+
+                        // Distribuimos o valor que sobrou
+                        //entre os elementos adjacentes
                         #pragma omp critcal 
                         {
                             if (j+1 < m && mat2[i][j+1][R] + delta > 255*MU)
@@ -45,9 +73,13 @@ int main (int argc, char** argv) {
                                 mat2[i-1][j][R].fetch_add (delta);
                         }
                     }
+                    // Se o valor do verde deu overflow
                     if (mat2[i][j][G] > 255 * MU) {
                         ull delta = mat2[i][j][G] - 255*MU / 4;
                         mat2[i][j][G].store (255*MU);
+
+                        // Distribuimos o valor que sobrou
+                        //entre os elementos adjacentes
                         #pragma omp critcal 
                         {
                             if (j+1 < m && mat2[i][j+1][G] + delta > 255*MU)
@@ -63,9 +95,13 @@ int main (int argc, char** argv) {
                                 mat2[i-1][j][G].fetch_add (delta);
                         }
                     }
+                    // Se o valor do azul deu overflow
                     if (mat2[i][j][B] > 255 * MU) {
                         ull delta = mat2[i][j][B] - 255*MU / 4;
                         mat2[i][j][B].store (255*MU);
+
+                        // Distribuimos o valor que sobrou
+                        //entre os elementos adjacentes
                         #pragma omp critcal 
                         {
                             if (j+1 < m && mat2[i][j+1][B] + delta > 255*MU)
@@ -84,7 +120,10 @@ int main (int argc, char** argv) {
                 }
         }
 
-
+        // Salvamos o conteúdo de mat2 na matriz mat e ajustamos
+        //o valor da componente verde de acordo com o valor do azul
+        //e do vermelho.
+        // Esse laço também é paralelizado
         #pragma omp parallel for schedule (guided, 4)
         for (int i = 0; i < n; i++)
             for (int j = 0; j < m; j++) {
@@ -94,6 +133,7 @@ int main (int argc, char** argv) {
                 mat[i][j][G].store (((ull) (angle * 255 * MU / (2 * M_PI)) + mat[i][j][G]) % (255 * MU));
             }
     }
+    // Imprimimos a imagem resultante
     print_im (argv[2]);
 
     return EXIT_SUCCESS;
--- ep.hpp
+++ ep.hpp
@@ -1,3 +1,11 @@
+/*******************************************************************/
+/*                    Projeto de MAC0431                           */
+/* Nomes:                                                          */
+/* Leonardo Padilha                             Nº USP: 9298295    */
+/* Lucas Santos                                 Nº USP:            */
+/* Victor da Matta                              Nº USP:            */
+/* Victor Raposo                                Nº USP:            */
+/*******************************************************************/
 #include <omp.h>
 #include <cstdio>
 #include <cstdlib>
@@ -5,16 +13,19 @@
 #include <iostream>
 #include <algorithm>
 #include <cmath>
+#include <string.h>
 
-#define MAX 4005
 
-#define MU 1000000
-#define R 0
-#define G 1
-#define B 2
+#define MAX 4005   /* Tamanho máximo de largura ou altura da imagem*/
+
+#define MU 1000000 /* Valor do multiplicador */
+#define R 0        /* Indíce para o vermelho */
+#define G 1        /* Indíce para o verde    */
+#define B 2        /* Indíce para o azul     */
 
 typedef unsigned long long ull;
 
+/* Variáveis globais */
 const int is[] = {0, 1, 0, -1};
 const int js[] = {1, 0, -1, 0};
 
@@ -22,24 +33,94 @@ int n, m, compM;
 std::atomic<ull> mat[MAX][MAX][3];
 std::atomic<ull> mat2[MAX][MAX][3];
 
+/* read_im: recebe uma string entrada e le a imagem em formato  */
+/* .ppm cujo nome é entrada e a salva na matriz global mat.     */
 void read_im (char* entrada) {
     FILE *in = fopen (entrada, "r");
+    int c, compare;
     char mn[2];
 
-    fscanf (in, "%s %d %d %d", &mn, &n, &m, &compM);
+    // Leitura do formato da imagem ('P3' ou 'P6')
+    if (fscanf (in, "%s ", mn) != 1) {
+        fprintf(stderr, "ERROR!\n");
+        exit (0);
+    }
+
+    if (strcmp (mn, "P6") != 0 && strcmp (mn, "P3") != 0) {
+        fprintf(stderr, "ERROR: invalid image format\n");
+        exit (0);
+    }
+
+    // Verificação de comentários
+    c = getc(in);
+    while (c == '#') {
+        while (getc(in) != '\n') ;
+        c = getc(in);
+    }
+    ungetc (c, in);
+
+    if (fscanf (in, "%d %d %d", &n, &m, &compM) != 3) {
+        fprintf(stderr, "ERROR: invalid image size!\n");
+        exit (0);
+    }
+
+    // Retirando os espaços em branco
+    c = getc(in);
+    while (c == ' ' || c == '\t' || c == '\n')
+        c = getc(in);
+    ungetc (c, in);
+
+    // Salvamos o valor da comparação do magic number com a string P6
+    compare = strcmp ("P6", mn); 
 
     for (int i = 0; i < n; i++)
         for (int j = 0; j < m; j++) {
-            int r, g, b;
-            fscanf (in, "%d %d %d", &r, &g, &b);
-            mat[i][j][R] = mat2[i][j][R] = r * MU;
-            mat[i][j][G] = mat2[i][j][G] = g * MU;
-            mat[i][j][B] = mat2[i][j][B] = b * MU;
+            int scan_ret = 0;
+
+            // Se o tipo da imagem for P6
+            if (compare == 0) {
+                unsigned char r, g, b;
+                scan_ret += fread(&r, 1, 1, in);
+                scan_ret += fread(&g, 1, 1, in);
+                scan_ret += fread(&b, 1, 1, in);
+
+                if (scan_ret != 3) {  
+                    fprintf(stderr, "ERROR: invalid pixel!\n");
+                    exit (0);
+                }
+
+
+                // Salvamos na matriz mat e mat2 o valor * MU
+                //para usar as operações atomicas do std::atomic
+                //sem precisar declarar como double
+                mat[i][j][R] = mat2[i][j][R] = r * MU;
+                mat[i][j][G] = mat2[i][j][G] = g * MU;
+                mat[i][j][B] = mat2[i][j][B] = b * MU;
+            }
+            // Se o tipo da imagem for P3
+            else {
+                int r, g, b;
+                scan_ret = fscanf (in, "%d %d %d", &r, &g, &b);
+
+                if (scan_ret != 3) {
+                    fprintf(stderr, "ERROR: invalid pixel!\n");
+                    exit (0);
+                }
+
+                // Salvamos na matriz mat e mat2 o valor * MU
+                //para usar as operações atomicas do std::atomic
+                //sem precisar declarar como double
+                mat[i][j][R] = mat2[i][j][R] = r * MU;
+                mat[i][j][G] = mat2[i][j][G] = g * MU;
+                mat[i][j][B] = mat2[i][j][B] = b * MU;
+            }
         }
 
     fclose (in);
 }
 
+/* print_im: recebe uma string saida e imprime uma imagem em formato */
+/*.ppm cujo conteúdo é o resultado da matriz mat.                    */
 void print_im (char *saida) {
     FILE *out = fopen (saida, "w");
 
@@ -48,6 +129,7 @@ void print_im (char *saida) {
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
             int r, g, b;
+            // Normalizamos os valores para 0-255
             r = mat[i][j][R] / MU;
             b = mat[i][j][G] / MU;
             g = mat[i][j][B] / MU;
@@ -60,6 +142,10 @@ void print_im (char *saida) {
     fclose (out);
 }
 
+/* operacao: recebe um inteiro i e um j e realiza a operação dada */
+/*no enunciado do projeto no pixel (i,j) da imagem. Retorna 0 se  */
+/*a operação foi executada corretamente ou 1 se houve overflow no */
+/*pixel.                                                          */
 int operacao (int i, int j) {
     double angle = 2 * M_PI * mat[i][j][G] / (255. * MU);
     double s = abs (sin (angle));
@@ -90,11 +176,14 @@ int operacao (int i, int j) {
     // Unroll
     int ret = 0;
 
+    // Alteramos o valor das cores de cada um dos pixels vizinhos
     if (j+1 < m) {
+        // delta é o valor a ser adicionado ao pixel
         ull delta = (255 * MU - mat[i][j+1][send_cor[0]]) * send[0] / (1020 * MU);
         mat2[i][j][send_cor[0]].fetch_sub (delta);
         mat2[i][j+1][send_cor[0]].fetch_add (delta);
 
+        // se houve overflow, retornamos 1
         if (mat2[i][j+1][send_cor[0]] > 255 * MU) ret = 1;
     }
 
